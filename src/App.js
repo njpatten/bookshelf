12544e678fa3551568fcc0367accae995ead08dd
Nicole Patten <njpatten@njpatten-macbookpro2.roam.corp.google.com>
2019-04-07 19:14:25 -0400

    manage update book

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import * as BooksAPI from './BooksAPI'
+import SearchPage from './SearchPage'
 import './App.css'
 
 import BookShelf from './BookShelf'
@@ -17,35 +19,61 @@ class BooksApp extends React.Component {
   }
 
   componentDidMount = () => {
-    BooksAPI.getAll().then((books) => this.setState({books: books}))
+    this.getBooks();
+  }
+
+  updateShelf = (shelf, book) => {
+    BooksAPI.update(book, shelf).then(this.getBooks());
+  }
+
+  componentDidUpdate(){
+    this.getBooks();
+  }
+
+  getBooks = () => {
+    BooksAPI.getAll().then((books) => this.setState({books: books}));
   }
 
   render() {
+    const { books } = this.state;
+
     return (
-      <div className="app">
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
+      <Router>
+        <div className="app">
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <BookShelf 
+                books={books.filter(book => book.shelf === 'currentlyReading')}
+                shelf='currentlyReading'
+                title='Currently Reading'
+                updateShelf={this.updateShelf}
+              />
+              <BookShelf 
+                books={books.filter(book => book.shelf === 'wantToRead')}
+                shelf='wantToRead'
+                title='Want to Read'
+                updateShelf={this.updateShelf}
+              />
+              <BookShelf
+                books={books.filter(book => book.shelf === 'read')}
+                shelf='read'
+                title='Read'
+                updateShelf={this.updateShelf}
+              />
+            </div>
+                
+            <div className="open-search">
+              <Link to='/search'>
+                <button>Add a book</button>
+              </Link>
+            </div>
           </div>
-          <div className="list-books-content">
-            <BookShelf 
-              books={this.state.books}
-              shelf='currentlyReading'
-              title='Currently Reading'
-            />
-            <BookShelf 
-              books={this.state.books}
-              shelf='wantToRead'
-              title='Want to Read'
-            />
-            <BookShelf />
-          </div>
-              
-          <div className="open-search">
-            <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-          </div>
+          <Route path="/search" exact component={SearchPage} />
         </div>
-      </div>
+      </Router>
     )
   }
 }
--- src/Book.js
+++ src/Book.js
@@ -1,18 +1,30 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 
 class Book extends React.Component {
   state = {
-    
+    selectedShelf: '',
   }
 
+  componentDidMount = () => {
+    BooksAPI.get(this.props.bookID).then((book) => console.log(book));
+  }
+
+  handleChange = (event) => {
+    let newShelf = event.target.value;
+    this.props.changeShelf(newShelf);
+  }
+
+
   render() {
+    const { thumbnail, title, authors, shelf } = this.props;
+
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${thumbnail}")` }}></div>
           <div className="book-shelf-changer">
-            <select>
+            <select value={shelf} onChange={(event) => this.handleChange(event)}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
@@ -21,8 +33,8 @@ class Book extends React.Component {
             </select>
           </div>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors.join(', ')}</div>
       </div>
     )
   }
